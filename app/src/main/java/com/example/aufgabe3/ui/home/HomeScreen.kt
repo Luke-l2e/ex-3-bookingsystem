82651d8ec291e661308e034e31c225d5bc501d64
Luke-l2e <overcharge2001@gmail.com>
2024-12-09 11:45:09 +0100

    Align "no booking entries" to center Refactor code

--- app/src/main/java/com/example/aufgabe3/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/aufgabe3/ui/home/HomeScreen.kt
@@ -1,16 +1,32 @@
 package com.example.aufgabe3.ui.home
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.aufgabe3.R
@@ -49,7 +65,11 @@ fun HomeScreen(
                 .fillMaxSize()
         ) {
             if (bookingsEntries.isEmpty()) {
-                Text(stringResource(R.string.no_booking_entries))
+                Text(
+                    stringResource(R.string.no_booking_entries),
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
             } else {
                 LazyColumn {
                     items(bookingsEntries) { bookingEntry ->
@@ -85,9 +105,8 @@ fun BookingEntryItem(
                     text = bookingEntry.name,
                     style = MaterialTheme.typography.titleMedium
                 )
-                val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(
-                    Locale.getDefault()
-                ).withZone(ZoneId.systemDefault())
+                val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+                    .withLocale(Locale.getDefault()).withZone(ZoneId.systemDefault())
                 val date = "${bookingEntry.arrivalDate.format(formatter)} - ${
                     bookingEntry.departureDate.format(formatter)
                 }"
